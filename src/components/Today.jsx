0920efa5b5d61ff366966f4fe5d061167e734013
Aneesh kumar maury <aneeshmaury.in@gmail.com>
2023-12-07 08:53:52 +0530

    littlebit

--- src/components/Today.jsx
+++ src/components/Today.jsx
@@ -2,7 +2,7 @@ let Today=()=>{
     let time= new Date();
         return <>
             <p> {time.toLocaleDateString()} - {time.toLocaleTimeString()}</p>
-       This is INDIAN Local  g time
+       This is INDIAN Localjhgfuj  g time
         </>
          
          
