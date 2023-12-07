d4dbc7838729428b6fdf1b1aff8463859a92534c
Aneesh kumar maury <aneeshmaury.in@gmail.com>
2023-12-07 08:23:48 +0530

    some change

--- src/components/Today.jsx
+++ src/components/Today.jsx
@@ -1,6 +1,12 @@
 let Today=()=>{
     let time= new Date();
-        return <p> {time.toLocaleDateString()} -
-         {time.toLocaleTimeString()}</p> 
-}
+        return <>
+            <p> {time.toLocaleDateString()} - {time.toLocaleTimeString()}</p>
+       This is INDIAN time
+        </>
+         
+         
+
+        
+}        
 export default Today;
